26f236d3e7b818a0687634f164091d0319d67338
Jason L Perry <jasper@ambethia.com>
2012-02-23 15:23:21 -0500

    Change announcement message

--- Shortwave.lua
+++ Shortwave.lua
@@ -3,7 +3,7 @@ f:SetScript("OnEvent", function(frame, event, ...)
   if event == "ACHIEVEMENT_EARNED" then
     local _, name, _, _, _, _, _, _, _, _, _, isGuildAch = GetAchievementInfo(select(1, ...))
     if not isGuildAch then
-      BNSetCustomMessage(UnitName("player").." has earned \""..name.."\"!")
+      BNSetCustomMessage("Achievement Unlocked: \""..name.."\"!")
     end
   end
 end)
